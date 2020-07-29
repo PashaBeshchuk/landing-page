26ce9dfd2cdb1dafab36f263c01caeced970a506
Pasha Beshchuk <pashabeshchuk1990@gmail.com>
2020-07-29 15:02:08 +0300

    Updates

---
+++ precache-manifest.a1ebbe5c5217408dd09cf6d0778245f4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93b04e05daf2efd7300c7518c8f5be44",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "b3aa907b29cb170486f1",
+    "url": "/landing-page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0d0dced14d3ef6e59fbf",
+    "url": "/landing-page/static/css/main.064a49d4.chunk.css"
+  },
+  {
+    "revision": "b3aa907b29cb170486f1",
+    "url": "/landing-page/static/js/2.9a63c123.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/landing-page/static/js/2.9a63c123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d0dced14d3ef6e59fbf",
+    "url": "/landing-page/static/js/main.456e8def.chunk.js"
+  },
+  {
+    "revision": "d803e7ff0721cd373432",
+    "url": "/landing-page/static/js/runtime-main.8c4e35fe.js"
+  },
+  {
+    "revision": "92ce97da21eeb962a8809be8a339a939",
+    "url": "/landing-page/static/media/art.92ce97da.jpg"
+  },
+  {
+    "revision": "1c855fb584eb82f84e7471d7a8e54510",
+    "url": "/landing-page/static/media/beach.1c855fb5.jpg"
+  },
+  {
+    "revision": "1b0453d2613eb6b48cc2c735a765b99e",
+    "url": "/landing-page/static/media/elephant2.1b0453d2.jpg"
+  },
+  {
+    "revision": "40501287766b673efcfb9effa02dfb10",
+    "url": "/landing-page/static/media/forest2.40501287.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/landing-page/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "40c05c38788c715a3e84529451218f3a",
+    "url": "/landing-page/static/media/manh-bridge.40c05c38.jpg"
+  },
+  {
+    "revision": "a864e2132366999baa6439ac00b90389",
+    "url": "/landing-page/static/media/mountains.a864e213.jpg"
+  },
+  {
+    "revision": "6715424dba939ad6fc6e10c8156cf02e",
+    "url": "/landing-page/static/media/river.6715424d.jpg"
+  },
+  {
+    "revision": "868239f001680a96101e44bc70f015fd",
+    "url": "/landing-page/static/media/road-trees.868239f0.jpg"
+  },
+  {
+    "revision": "2f01fbca013b24b7f4d4136347a8c49e",
+    "url": "/landing-page/static/media/snowboard.2f01fbca.jpg"
+  }
+]);
